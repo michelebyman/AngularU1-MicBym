7db61ad58ca6417bbc1e84d9c9841afadf1854e8
michele byman <michelebyman@gmail.com>
2019-02-25 14:51:10 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserListItemComponent } from './user-list-item/user-list-item.component';
+import { EditUsersComponent } from './edit-users/edit-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UserListComponent,
+    UserListItemComponent,
+    EditUsersComponent
   ],
   imports: [
     BrowserModule,
